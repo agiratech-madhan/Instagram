5bfdea889999edd73562c92e97b031829b85dbfe
agiratech-madhan <madhan.k@agiratech.com>
2023-05-17 13:10:07 +0530

    initial commit

---
+++ my_instagram/lib/src/services/services_initializer.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+import '../../firebase_options.dart';
+import 'local_storage/key_value_storage_base.dart';
+
+final servicesInitializerProvider = Provider<ServicesInitializer>((ref) {
+  return ServicesInitializer(ref);
+});
+
+class ServicesInitializer {
+  ServicesInitializer(this.ref);
+  final Ref ref;
+
+  Future<void> init() async {
+    await _initKeyValueStorage();
+    await _initFirebase();
+  }
+
+  Future<void> _initFirebase() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    //! Need Setup a Foreground Push Notification
+  }
+
+  Future<void> _initKeyValueStorage() async {
+    await KeyValueStorageBase.init();
+  }
+}
---
+++ my_instagram/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,18 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import connectivity_plus
+import firebase_core
+import flutter_secure_storage_macos
+import shared_preferences_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+}
